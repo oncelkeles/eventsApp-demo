327e05134f995ce595da623e7a680dd0a6ca0382
oncelkeles <keles.oncel@gmail.com>
2021-03-08 15:14:30 +0300

    Delete old files

--- readme.md
+++
@@ -1,3 +0,0 @@
-# Events App
-
-The backend of the app, built using nodejs, express, mongoDB, mongoose and typescript.
--- utils/catchAsync.ts
+++
@@ -1,7 +0,0 @@
-import { RequestHandler } from "express";
-
-export const catchAsync: Function = (fn: Function): RequestHandler => {
-  return (req, res, next) => {
-    fn(req, res, next).catch((err: Error) => next(err));
-  };
-};
